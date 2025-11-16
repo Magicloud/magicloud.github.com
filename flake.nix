974409351310d57e9c3fc21738add82df1a17343
Magicloud <1886157+Magicloud@users.noreply.github.com>
2025-11-16 21:02:45 +0800

    nix develop

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Jekyll Github Pages Env";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: let
+    system = "x86_64-linux";
+  in {
+    devShells."${system}".default = let
+      pkgs = import nixpkgs { inherit system; };
+    in pkgs.mkShell {
+      packages = with pkgs; [ ruby zlib.dev libffi.dev libyaml.dev ];
+      shellHook = ''
+        exec zsh
+      '';
+    };
+  };
+}
